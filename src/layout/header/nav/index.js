749ada8a09d4b061d892dcef7f373ab8d4a2e279
hodasalah <hodasalah35@gmail.com>
2021-06-10 14:52:43 +0200

    add navigation functionality

--- src/layout/header/nav/index.js
+++ src/layout/header/nav/index.js
@@ -1,44 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import Backdrop from "../../../components/UI/backdrop";
-import List from "./List";
+import { NavLink } from "react-router-dom";
 
 import "./style.scss";
 
 const Nav = (props) => {
-	const firstUL = [
-		"Bedingungen",
-		"Behandlungen",
-		"Veranstaltungen",
-		"Werkzeuge",
-		"Forschung und Erkenntnisse",
-	];
-	const SecUL = [
-		"Adtralza",
-		"Diavonex",
-		"Diavobet",
-		"Enstilar",
-		"Fucidin",
-		"Kyntheum",
-		"Protopic",
-		"Skinoren",
-		"Tralokinumab",
-		"Xamiol",
-	];
-	const thirdUL = [
-		"Overview",
-		"Mode of Action",
-		"Efficacy",
-		"Quality of Life",
-		"Safety",
-		"Dosing",
-		"News",
-		"	Technical Information",
-	];
+	console.log(props);
+	const mainNav = {
+		items: [
+			"Bedingungen",
+			"Behandlungen",
+			"Veranstaltungen",
+			"Werkzeuge",
+			"Forschung und Erkenntnisse",
+		],
+	};
+	const SecUL = {
+		id: "Behandlungen",
+		items: [
+			"Adtralza",
+			"Diavonex",
+			"Diavobet",
+			"Enstilar",
+			"Fucidin",
+			"Kyntheum",
+			"Protopic",
+			"Skinoren",
+			"Tralokinumab",
+			"Xamiol",
+		],
+	};
+	const thirdUL = {
+		items: [
+			"Overview",
+			"Mode of Action",
+			"Efficacy",
+			"Quality of Life",
+			"Safety",
+			"Dosing",
+			"News",
+			"	Technical Information",
+		],
+	};
+	// second nav is related to second link in nav 1
+	// so i define id for second nav to show it just when
+	// we click in second link in nav1
+	const [item, setItem] = useState("");
+	const [subItem, setSubItem] = useState("");
+	const [isActive, setIsActive] = useState(false);
+	const [show, setShow] = useState(false);
+
+	const click = (item) => {
+		setItem(item);
+		setIsActive(true);
+	};
+	const showThird = (it) => {
+		setSubItem(it);
+		setShow(true);
+	};
 
 	return (
 		<>
 			<Backdrop show={props.open} />,
-			<nav className={[props.open ? "active" : "close"]}>
+			<nav className={props.open ? "active" : "close"}>
 				{props.open ? (
 					<button
 						onClick={() => props.closeHandelar(false)}
@@ -48,14 +72,45 @@ const Nav = (props) => {
 					</button>
 				) : null}
 				<ul className="nav_first">
-					<List arr={firstUL} />
+					{mainNav.items.map((el) => (
+						<li key={el}>
+							<NavLink
+								to={`/${el}`}
+								activeClassName="active"
+								onClick={() => click(el)}
+							>
+								{el}
+							</NavLink>
+						</li>
+					))}
 				</ul>
-				<ul className="nav_sec grey-bg">
-					<List arr={SecUL} />
-				</ul>
-				<ul className="nav_third blue-bg">
-					<List arr={thirdUL} />
-				</ul>
+				{isActive && item === SecUL.id && (
+					<ul className="nav_sec grey-bg">
+						{SecUL.items.map((el) => (
+							<li key={el}>
+								<NavLink
+									to={`/${item}/${el}`}
+									activeClassName="active"
+									onClick={() => showThird(el)}
+								>
+									{el}
+								</NavLink>
+							</li>
+						))}
+					</ul>
+				)}
+
+				{show && item === SecUL.id ? (
+					<ul className="nav_third blue-bg">
+						{thirdUL.items.map((li) => (
+							<li key={li}>
+								<NavLink to={`/${item}/${subItem}/${li}`}>
+									{li}
+								</NavLink>
+							</li>
+						))}
+					</ul>
+				) : null}
 			</nav>
 		</>
 	);
